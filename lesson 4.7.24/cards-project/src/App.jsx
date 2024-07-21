efb50bc620ea0e40a1d5b7531283c416b346b069
niveenbader <neveen.bader@gmail.com>
2024-07-22 00:03:08 +0300

    update

--- lesson 4.7.24/cards-project/src/App.jsx
+++ lesson 4.7.24/cards-project/src/App.jsx
@@ -1,15 +1,37 @@
-import BusinessCard from './components/businessCard';
-import './App.css'
+import BusinessCard from "./components/businessCard";
+import "./App.css";
 
 function App() {
-
-
-  return (<>
-    <><BusinessCard /></>
-    <><BusinessCard /></>
-  </>
+  return (
+    <>
+      <BusinessCard
+        logoUrl="https://th.bing.com/th/id/OIP.qNllopTe-sGyXx04HQCFTwHaHa?rs=1&pid=ImgDetMain"
+        name="Revive Israel"
+        description="messianic ministry"
+        address="Yad Hashmona"
+        phone="05053926168"
+        website="https://reviveisrael.org/"
+        email="webmanager@reviveisrael.org"
+      />
+      <BusinessCard
+        logoUrl="https://th.bing.com/th/id/OIP.qNllopTe-sGyXx04HQCFTwHaHa?rs=1&pid=ImgDetMain"
+        name="revive Israel"
+        description="messianic ministry"
+        address="Yad Hashmona"
+        phone="05053926168"
+        website="https://reviveisrael.org/"
+        email="webmanager@reviveisrael.org"
+      />
+      <BusinessCard
+        logoUrl="https://th.bing.com/th/id/OIP.qNllopTe-sGyXx04HQCFTwHaHa?rs=1&pid=ImgDetMain"
+        name="revive Israel"
+        description="messianic ministry"
+        address="Yad Hashmona"
+        website="https://reviveisrael.org/"
+        email="webmanager@reviveisrael.org"
+      />
+    </>
   );
 }
 
-export default App
-
+export default App;
--- lesson 4.7.24/cards-project/src/components/businessCard.jsx
+++ lesson 4.7.24/cards-project/src/components/businessCard.jsx
@@ -1,42 +1,54 @@
-
-function BusinessCard(props) {
-    return (
-        <div>
-            <div className="card" style={{ width: "18rem" }}>
-                <img src={props.logoUrl} className="card-img-top" alt={`${name} logo`} />
-                <div className="card-body">
-                    <h5 className="card-title">{props.name}</h5>
-                    <p className="card-text">{props.description}</p>
-                </div>
-                <ul className="list-group list-group-flush">
-                    <li className="list-group-item">
-                        <span className="icon me-3">
-                            <i className="bi bi-building-fill"></i>
-                        </span>
-                        <span >{props.address}</span>
-                    </li>
-                    <li className="list-group-item">
-                        <span className="icon me-3">
-                            <i className="bi bi-telephone-fill"></i>
-                        </span>
-                        <span>{props.phone}</span>
-                    </li>
-                    <li className="list-group-item">
-                        <span className="icon me-3">
-                            <i className="bi bi-globe"></i>
-                        </span>
-                        <span>{props.website}</span>
-                    </li>
-                    <li className="list-group-item">
-                        <span className="icon me-3">
-                            <i className="bi bi-envelope-fill"></i>
-                        </span>
-                        <span>{props.email}</span>
-                    </li>
-                </ul>
-            </div>
+function BusinessCard({
+  logoUrl,
+  name,
+  description,
+  address,
+  phone,
+  website,
+  email,
+}) {
+  return (
+    <div>
+      <div className="card" style={{ width: "18rem" }}>
+        <img src={logoUrl} className="card-img-top" alt={`${name} logo`} />
+        <div className="card-body">
+          <h5 className="card-title">
+            {name[0].toUpperCase() + name.slice(1)}
+          </h5>
+          <p className="card-text">{description}</p>
         </div>
-    )
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <span className="icon me-3">
+              <i className="bi bi-building-fill"></i>
+            </span>
+            <span>{address}</span>
+          </li>
+          {phone ? (
+            <li className="list-group-item">
+              <span className="icon me-3">
+                <i className="bi bi-telephone-fill"></i>
+              </span>
+              <span>{phone}</span>
+            </li>
+          ) : null}
+
+          <li className="list-group-item">
+            <span className="icon me-3">
+              <i className="bi bi-globe"></i>
+            </span>
+            <span>{website}</span>
+          </li>
+          <li className="list-group-item">
+            <span className="icon me-3">
+              <i className="bi bi-envelope-fill"></i>
+            </span>
+            <span>{email}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default BusinessCard;
