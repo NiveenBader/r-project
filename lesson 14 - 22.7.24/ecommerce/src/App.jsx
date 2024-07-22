8656f57e9cb52f7baef88d7fb7d1dd8ab71d300f
niveenbader <neveen.bader@gmail.com>
2024-07-22 20:29:19 +0300

    ecommerce project

---
+++ lesson 14 - 22.7.24/ecommerce/src/App.jsx
@@ -0,0 +1,15 @@
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import "./App.css";
+import Shop from "./components/shop.jsx";
+
+function App() {
+  return (
+    <>
+      <Shop />
+    </>
+  );
+}
+
+export default App;
