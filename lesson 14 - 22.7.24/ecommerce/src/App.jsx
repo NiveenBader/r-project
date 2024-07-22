b6c08b866079624ead4bac9da6f4e3cbdd45d419
niveenbader <neveen.bader@gmail.com>
2024-07-22 21:04:27 +0300

    update

--- lesson 14 - 22.7.24/ecommerce/src/App.jsx
+++ lesson 14 - 22.7.24/ecommerce/src/App.jsx
@@ -2,7 +2,8 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 import "./App.css";
-import Shop from "./components/shop.jsx";
+
+import Shop from "./components/Shop";
 
 function App() {
   return (
--- lesson 14 - 22.7.24/ecommerce/src/components/shop.jsx
+++ lesson 14 - 22.7.24/ecommerce/src/components/shop.jsx
@@ -2,7 +2,7 @@ function Shop() {
   return (
     <div className="container">
       <div className="card" style={{ width: "18 rem" }}>
-        <img src="..." class="card-img-top" alt="..." />
+        <img src="..." className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">Card title</h5>
           <p className="card-text">
